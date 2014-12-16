3aabd2021d80ca2f3b1ef8e7d2fcb4c5f087f7bd
redss <ryszard.tarajkowski@gmail.com>
2014-12-16 23:26:36 +0100

    KeyboardHook missing tests.

--- ShortcutRunner.Tests/KeyboardHookTests.cs
+++ ShortcutRunner.Tests/KeyboardHookTests.cs
@@ -12,15 +12,12 @@ namespace ShortcutRunner.Tests
             // Arrange
 
             var fixture = new KeyboardHookFixture();
-
             var sut = fixture.CreateSut();
-
             var windowIntPtr = new IntPtr(123);
+            var input = new ShortcutDescription();
 
             A.CallTo(() => fixture.MessageCatchingWindow.Handle)
                 .Returns(windowIntPtr);
-
-            var input = new ShortcutDescription();
 
             // Act
 
@@ -29,6 +26,68 @@ namespace ShortcutRunner.Tests
             // Assert
 
             A.CallTo(() => fixture.KeyRegistrationController.RegisterHotKey(windowIntPtr, input))
+                .MustHaveHappened();
+        }
+
+        [Test]
+        public void Can_Notify_About_Hotkey_Event()
+        {
+            // Arrange
+
+            var fixture = new KeyboardHookFixture();
+            var sut = fixture.CreateSut();
+
+            var observer = A.Fake<EventHandler<KeyPressedEventArgs>>();
+            var eventArgs = new KeyPressedEventArgs(new ShortcutDescription());
+
+            // Act
+
+            sut.KeyPressed += observer;
+            fixture.MessageCatchingWindow.KeyPressed += Raise.With(eventArgs);
+
+            // Assert
+
+            A.CallTo(() => observer(sut, eventArgs))
+                .MustHaveHappened();
+        }
+
+        [Test]
+        public void Can_Work_Without_Registered_Event_Listeners()
+        {
+            // Arrange
+
+            var fixture = new KeyboardHookFixture();
+            var sut = fixture.CreateSut();
+
+            var eventArgs = new KeyPressedEventArgs(new ShortcutDescription());
+
+            // Act
+
+            fixture.MessageCatchingWindow.KeyPressed += Raise.With(eventArgs);
+
+            // Assert
+
+            Assert.Pass("Works, when no event listeners were registered.");
+        }
+
+        [Test]
+        public void Can_Dispose_Window_And_Key_Registration_Controller()
+        {
+            // Arrange
+
+            var fixture = new KeyboardHookFixture();
+            var sut = fixture.CreateSut();
+
+            // Act
+
+            sut.Dispose();
+
+            // Assert
+
+            A.CallTo(() => fixture.MessageCatchingWindow.Dispose())
+                .MustHaveHappened();
+
+            A.CallTo(() => fixture.KeyRegistrationController.Dispose())
                 .MustHaveHappened();
         }
     }
