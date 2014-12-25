04970d38f2e802752fd734fa5e5f585d93325843
redss <ryszard.tarajkowski@gmail.com>
2014-12-25 10:16:02 +0100

    ShortcutDescriptionParser further implementation.

--- ShortcutRunner.Tests/ShortcutDescriptionParserTests.cs
+++ ShortcutRunner.Tests/ShortcutDescriptionParserTests.cs
@@ -9,36 +9,47 @@ namespace ShortcutRunner.Tests
     {
         public ShortcutDescriptionParser Sut = new ShortcutDescriptionParser();
 
-        [Test]
-        public void Can_Parse_Shortcut_Description()
+        public class ParseTestCase
         {
-            var result = Sut.Parse("Ctrl + Shift + C");
-
-            Assert.That(result.Modifiers, Is.EqualTo(ModifierKeys.Ctrl | ModifierKeys.Shift));
-            Assert.That(result.Key, Is.EqualTo(Keys.C));
+            public string ShortcutText { get; set; }
+            public ShortcutDescription ExpectedResult { get; set; }
         }
 
-        [Test]
-        public void Can_Parse_Other_Description()
+        [Datapoints]
+        public ParseTestCase[] TestCases =
         {
-            var result = Sut.Parse("Alt + Shift + F2");
+            new ParseTestCase
+            {
+                ShortcutText = "Ctrl + Shift + C",
+                ExpectedResult = new ShortcutDescription(ModifierKeys.Ctrl | ModifierKeys.Shift, Keys.C)
+            },
+            new ParseTestCase
+            {
+                ShortcutText = "Alt + Shift + F12",
+                ExpectedResult = new ShortcutDescription(ModifierKeys.Alt | ModifierKeys.Shift, Keys.F12)
+            }
+        };
 
-            Assert.That(result.Modifiers, Is.EqualTo(ModifierKeys.Alt | ModifierKeys.Shift));
-            Assert.That(result.Key, Is.EqualTo(Keys.F2));
+        [TestCaseSource("TestCases")]
+        public void Can_Parse_Shortcut_Description(ParseTestCase testCase)
+        {
+            var result = Sut.Parse(testCase.ShortcutText);
+
+            Assert.That(result, Is.EqualTo(testCase.ExpectedResult));
         }
 
         [Test]
         public void Throws_Exception_When_Non_Modifier_Keys_Are_Used_More_Than_Once()
         {
             Assert.That(() => Sut.Parse("Ctrl + A + B"),
-                Throws.TypeOf<ArgumentException>());
+                Throws.TypeOf<MultipleNonModifierKeysException>());
         }
 
         [Test]
-        public void Throws_Exception_When_Input_Is_Invalid()
+        public void Throws_Exception_When_Key_Is_Invalid()
         {
-            Assert.That(() => Sut.Parse("invalid shortcut descirption"),
-                Throws.TypeOf<ArgumentException>());
+            Assert.That(() => Sut.Parse("Ctrl + Invalid Key"),
+                Throws.TypeOf<KeyNotRecognizedException>()); // todo check invalid key string
         }
 
         [Test]
@@ -46,6 +57,13 @@ namespace ShortcutRunner.Tests
         {
             Assert.That(() => Sut.Parse(null),
                 Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Throws_Exception_When_There_Is_No_Non_Modifier_Key()
+        {
+            Assert.That(() => Sut.Parse("Ctrl + Shift"),
+                Throws.TypeOf<NoNonModifierKeysException>());
         }
     }
 }
--- ShortcutRunner/ShortcutDescriptionParser.cs
+++ ShortcutRunner/ShortcutDescriptionParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using ShortcutRunner.HotkeyRegistration;
@@ -10,8 +11,39 @@ namespace ShortcutRunner
         ShortcutDescription Parse(string shortcut);
     }
 
+    public class ShortcutParsingException : Exception
+    {
+    }
+
+    public class NoNonModifierKeysException : ShortcutParsingException
+    {
+    }
+
+    public class KeyNotRecognizedException : ShortcutParsingException
+    {
+        public string NotRecognizedKey { get; set; }
+    }
+
+    public class MultipleNonModifierKeysException : ShortcutParsingException
+    {
+    }
+
     public class ShortcutDescriptionParser : IShortcutDescriptionParser
     {
+        interface IParsedKey
+        {
+        }
+
+        class ParsedModifier : IParsedKey
+        {
+            public ModifierKeys ModifierKeys { get; set; }
+        }
+
+        class ParsedKey : IParsedKey
+        {
+            public Keys Key { get; set; }
+        }
+
         public ShortcutDescription Parse(string shortcut)
         {
             if (shortcut == null)
@@ -19,38 +51,53 @@ namespace ShortcutRunner
                 throw new ArgumentNullException("shortcut");
             }
 
-            var parts = shortcut.Split('+').Select(s => s.Trim()).ToArray();
+            var parts = GetShortcutParts(shortcut)
+                .Select(ParseKey)
+                .ToArray();
 
-            var modifiers = ModifierKeys.None;
-            var keys = Keys.None;
-
-            foreach (var part in parts)
+            if (!parts.OfType<ParsedKey>().Any())
             {
-                var modifier = TryParseModifier(part);
-
-                if (modifier.HasValue)
-                {
-                    modifiers |= modifier.Value;
-                    continue;
-                }
-
-                var key = TryParseKey(part);
-
-                if (key.HasValue && keys != Keys.None)
-                {
-                    throw new ArgumentException("Shortcut description is invalid: more then one non-modifier key is used.", "shortcut");
-                }
-
-                if (key.HasValue)
-                {
-                    keys = key.Value;
-                    continue;
-                }
-
-                throw new ArgumentException("Shortcut description is invalid.", "shortcut");
+                throw new NoNonModifierKeysException();
             }
 
-            return new ShortcutDescription(modifiers, keys);
+            if (parts.OfType<ParsedKey>().Count() > 1)
+            {
+                throw new MultipleNonModifierKeysException();
+            }
+
+            var modifierKeys = parts
+                .OfType<ParsedModifier>()
+                .Select(p => p.ModifierKeys)
+                .Aggregate((a, b) => a | b);
+
+            var key = parts
+                .OfType<ParsedKey>()
+                .Single()
+                .Key;
+
+            return new ShortcutDescription(modifierKeys, key);
+        }
+
+        private IEnumerable<string> GetShortcutParts(string shortcut)
+        {
+            return shortcut.Split('+').Select(s => s.Trim());
+        }
+
+        private IParsedKey ParseKey(string keyString)
+        {
+            var modifier = TryParseModifier(keyString);
+            var key = TryParseKey(keyString);
+
+            if (modifier.HasValue)
+            {
+                return new ParsedModifier { ModifierKeys = modifier.Value };
+            }
+            if (key.HasValue)
+            {
+                return new ParsedKey { Key = key.Value };
+            }
+
+            throw new KeyNotRecognizedException{ NotRecognizedKey = keyString };
         }
 
         private ModifierKeys? TryParseModifier(string key)
