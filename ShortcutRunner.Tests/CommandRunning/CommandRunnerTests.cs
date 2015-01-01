bfa133036f1ea75404dd29932aeca1258929dc60
redss <ryszard.tarajkowski@gmail.com>
2015-01-01 17:06:46 +0100

    Added CommandRunner.

---
+++ ShortcutRunner.Tests/CommandRunning/CommandRunnerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using ShortcutRunner.CommandRunning;
+
+namespace ShortcutRunner.Tests.CommandRunning
+{
+    class CommandRunnerTests
+    {
+        [Test]
+        public void Can_Run_Command()
+        {
+            var sut = SutFactory.Create<CommandRunner>();
+
+            var process = sut.RunCommand("exit 123");
+
+            process.WaitForExit(1000);
+
+            Assert.That(process.ExitCode, Is.EqualTo(123));
+        }
+    }
+}
---
+++ ShortcutRunner/CommandRunning/CommandRunner.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace ShortcutRunner.CommandRunning
+{
+    public interface ICommandRunner
+    {
+        Process RunCommand(string command);
+    }
+
+    public class CommandRunner : ICommandRunner
+    {
+        public Process RunCommand(string command)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = "/C " + command // /C Carries out the command specified by string and then terminates
+            };
+
+            var process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+
+            return process;
+        }
+    }
+}
