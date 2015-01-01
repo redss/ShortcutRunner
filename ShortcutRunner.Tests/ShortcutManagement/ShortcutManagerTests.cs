3d88080eba8a1c8b171c5509e5fb941fe68c528e
redss <ryszard.tarajkowski@gmail.com>
2015-01-01 18:00:57 +0100

    Added shortcut management.

---
+++ ShortcutRunner.Tests/ShortcutManagement/ShortcutManagerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using FakeItEasy;
+using NUnit.Framework;
+using ShortcutRunner.ConfigurationParsing;
+using ShortcutRunner.HotkeyRegistration;
+using ShortcutRunner.ShortcutManagement;
+
+namespace ShortcutRunner.Tests.ShortcutManagement
+{
+    class ShortcutManagerTests
+    {
+        [Test]
+        public void Can_Initialize_Shortcuts()
+        {
+            // Arrange
+
+            var sut = SutFactory.Create<ShortcutManager>();
+
+            var firstLine = new ConfigurationLine
+            {
+                Shortcut = new ShortcutDescription(ModifierKeys.Alt, Keys.Z)
+            };
+
+            var secondLine = new ConfigurationLine
+            {
+                Shortcut = new ShortcutDescription(ModifierKeys.Alt, Keys.Y)
+            };
+
+            A.CallTo(() => sut.ConfigurationManager.ReadConfigurationFile())
+                .Returns(new[] { firstLine, secondLine });
+
+            // Act
+
+            sut.Initialize();
+
+            // Assert
+
+            // TODO: Test actions somehow.
+
+            A.CallTo(() => sut.ShortcutController.RegisterShortcutAction(firstLine.Shortcut, A<Action>._))
+                .MustHaveHappened();
+
+            A.CallTo(() => sut.ShortcutController.RegisterShortcutAction(secondLine.Shortcut, A<Action>._))
+                .MustHaveHappened();
+        }
+    }
+}
---
+++ ShortcutRunner/ShortcutManagement/ShortcutManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ShortcutRunner.CommandRunning;
+using ShortcutRunner.ConfigurationManagement;
+
+namespace ShortcutRunner.ShortcutManagement
+{
+    public interface IShortcutManager
+    {
+        void Initialize();
+    }
+
+    public class ShortcutManager : IShortcutManager
+    {
+        public readonly IConfigurationManager ConfigurationManager;
+        public readonly IShortcutController ShortcutController;
+        public readonly ICommandRunner CommandRunner;
+
+        public ShortcutManager(IConfigurationManager configurationManager, IShortcutController shortcutController, ICommandRunner commandRunner)
+        {
+            ConfigurationManager = configurationManager;
+            ShortcutController = shortcutController;
+            CommandRunner = commandRunner;
+        }
+
+        public void Initialize()
+        {
+            var configurationLines = ConfigurationManager.ReadConfigurationFile();
+
+            foreach (var line in configurationLines)
+            {
+                var command = line.Command;
+                Action action = () => CommandRunner.RunCommand(command);
+
+                ShortcutController.RegisterShortcutAction(line.Shortcut, action);
+            }
+        }
+    }
+}
